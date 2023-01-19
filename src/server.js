ca2339daa1601459c2d4a4251733c98fb1d7b1d5
Whitney <designedbywh@gmail.com>
2023-01-19 11:22:16 -0500

    add post request for nodemailer

--- src/server.js
+++ src/server.js
@@ -10,9 +10,43 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req,res) => {
-  res.send("Hello World!")
-})
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  host: "smtp.gmail.com",
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.EMAIL_PWD
+  }
+});
+
+transporter.verify((error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Ready to Send");
+  }
+});
+
+app.post("/", (req, res) => {
+  const {name, email, message} = req.body;
+
+  const mailOptions = {
+    from: email,
+    to: process.env.EMAIL,
+    subject: `New message from ${name}`,
+    text: message
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.status(500).json({ message: 'Error sending email' });
+    } else {
+      console.log(`Email sent: ${info.response}`);
+      res.json({ message: `Thanks for your message, ${name}! We will get back to you shortly.` });
+    }
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on PORT ${PORT}!`)
