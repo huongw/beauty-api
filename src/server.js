0737a14a3f38e249094b4ec00a461007c4ecf247
Whitney <designedbywh@gmail.com>
2023-01-24 12:38:35 -0500

    comment out nodemailer code

--- src/server.js
+++ src/server.js
@@ -11,29 +11,29 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const transporter = nodemailer.createTransport({
-  service: process.env.EMAIL_SERVICE,
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
-  auth: {
-    type: process.env.TYPE,
-    user: process.env.EMAIL,
-    pass: process.env.EMAIL_PWD,
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    refreshToken: process.env.REFRESH_TOKEN,
-    accessToken: process.env.ACCESS_TOKEN
-  }
-});
+// const transporter = nodemailer.createTransport({
+//   service: process.env.EMAIL_SERVICE,
+//   host: "smtp.gmail.com",
+//   port: 465,
+//   secure: true,
+//   auth: {
+//     type: process.env.TYPE,
+//     user: process.env.EMAIL,
+//     pass: process.env.EMAIL_PWD,
+//     clientId: process.env.CLIENT_ID,
+//     clientSecret: process.env.CLIENT_SECRET,
+//     refreshToken: process.env.REFRESH_TOKEN,
+//     accessToken: process.env.ACCESS_TOKEN
+//   }
+// });
  
-transporter.verify((error) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log("Ready to Send");
-  }
-});
+// transporter.verify((error) => {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log("Ready to Send");
+//   }
+// });
 
 app.get("/", (req, res) => {
   const data = ["hello", "good morning", "yoo"]
@@ -44,22 +44,24 @@ app.get("/", (req, res) => {
 app.post("/", (req, res) => {
   const {name, email, message} = req.body;
 
-  const mailOptions = {
-    from: email, 
-    to: process.env.EMAIL,
-    subject: `New message from ${name}`,
-    text: message
-  };
+  res.json({ message: `Thanks for your message, ${name}! We will get back to you shortly.` });
 
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.log(error);
-      res.json({ message: 'Error sending email! Please try again later' });
-    } else {
-      console.log(`Email sent: ${info.response}`);
-      res.json({ message: `Thanks for your message, ${name}! We will get back to you shortly.` });
-    }
-  });
+  // const mailOptions = {
+  //   from: email, 
+  //   to: process.env.EMAIL,
+  //   subject: `New message from ${name}`,
+  //   text: message
+  // };
+
+  // transporter.sendMail(mailOptions, (error, info) => {
+  //   if (error) {
+  //     console.log(error);
+  //     res.json({ message: 'Error sending email! Please try again later' });
+  //   } else {
+  //     console.log(`Email sent: ${info.response}`);
+  //     res.json({ message: `Thanks for your message, ${name}! We will get back to you shortly.` });
+  //   }
+  // });
 });
 
 app.listen(PORT, () => {
