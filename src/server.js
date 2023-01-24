34d697373190f5c4d7ede85ce6c40cfbce3faec4
Whitney <designedbywh@gmail.com>
2023-01-24 17:37:51 -0500

    install body parser and use gmail again

--- src/server.js
+++ src/server.js
@@ -3,17 +3,20 @@ dotenv.config();
 
 const nodemailer = require("nodemailer");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const PORT = process.env.PORT || 5000;
 const express = require("express");
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 app.use(express.json());
 app.use(cors());
 
 const transporter = nodemailer.createTransport({
-  host: 'smtp.ethereal.email',
-  port: 587,
+  service: process.env.EMAIL_SERVICE,
+  host: process.env.EMAIL_HOST,
   auth: {
     user: process.env.EMAIL,
     pass: process.env.EMAIL_PWD
@@ -44,8 +47,6 @@ app.post("/", (req, res) => {
       res.json({ message: "Cannot send email right now. Please try again later!" });
     } else {
       console.log(`Email sent: ${info.response}`);
-      console.log('Message sent: %s', info.messageId);
-      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
       res.json({ message: `Thanks for your message, ${name}! We will get back to you shortly.` });
     }
   });
