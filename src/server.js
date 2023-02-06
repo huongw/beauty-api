5e89a7de24ac31d81f319dc52b1bf75e40121bea
Whitney <designedbywh@gmail.com>
2023-02-06 12:35:37 -0500

    uninstall body-parser

--- src/server.js
+++ src/server.js
@@ -3,14 +3,11 @@ dotenv.config();
 
 const nodemailer = require("nodemailer");
 const cors = require("cors");
-const bodyParser = require("body-parser");
 
 const PORT = process.env.PORT || 5000;
 const express = require("express");
 const app = express();
 
-// app.use(bodyParser.urlencoded({extended: true}));
-// app.use(bodyParser.json());
 app.use(express.json());
 app.use(cors());
 
