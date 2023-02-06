d3cb5c3f6ab257931ab8a57c4bcf99f0408f1cfd
Whitney <designedbywh@gmail.com>
2023-02-06 12:29:55 -0500

    comment out body parser

--- src/server.js
+++ src/server.js
@@ -9,8 +9,8 @@ const PORT = process.env.PORT || 5000;
 const express = require("express");
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: true}));
+// app.use(bodyParser.json());
 app.use(express.json());
 app.use(cors());
 
